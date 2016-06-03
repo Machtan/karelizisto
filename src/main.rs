1e64a2325f72c0d18b369761c970dbf182b980c7
Machtan <jako3047@gmail.com>
2016-06-03 18:06:33 +0200

    Initial commit. Argument parsing added

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+extern crate space_toml;
+extern crate argonaut;
+
+use std::env;
+use argonaut::{ArgDef, Parse};
+
+const USAGE: &'static str = "Usage: cargo run \
+    [--help | OPTIONS ] schema";
+
+const HELP: &'static str = "\
+Required arguments:
+    schema                  A TOML file describing tiles and layers.
+
+Optional arguments:
+    --help | -h         Show this message.
+    --version           Show the version of this library.
+    --load PATH         Load this TOML file as a level.
+    --save PATH         Save the level to this path.
+    --edit PATH         The same as --load PATH --save PATH.
+";
+
+fn start_editor(schema: &str, load_from: Option<&str>, save_to: Option<&str>) {
+    println!("START!");
+}
+
+fn main() {
+    use argonaut::Arg::*;
+
+    // Create the arguments
+    let a_schema    = ArgDef::positional("schema");
+    let a_version   = ArgDef::named("version").switch();
+    let a_help      = ArgDef::named_and_short("help", 'h').switch();
+    let a_load      = ArgDef::named("load").option();
+    let a_save      = ArgDef::named("save").option();
+    let a_edit      = ArgDef::named("edit").option();
+    
+    let args: Vec<_> = env::args().skip(1).collect();
+    
+    // Prepare the options
+    let mut schema = "";
+    let mut load = None;
+    let mut save = None;
+    let mut edit = None;
+    
+    let expected = &[a_schema, a_version, a_help, a_load, a_save, a_edit];
+    
+    let mut parse = Parse::new(expected, &args).expect("Invalid definitions");
+    while let Some(item) = parse.next() {
+        match item {
+            Err(_) => {
+                //println!("Parse error: {:?}", err);
+                println!("{}", USAGE);
+                return;
+            },
+            Ok(Positional("schema", value)) => {
+                schema = value;
+            },
+            Ok(Option("load", value)) => {
+                load = Some(value);
+            },
+            Ok(Option("save", value)) => {
+                save = Some(value);
+            },
+            Ok(Option("edit", value)) => {
+                edit = Some(value);
+            },
+            Ok(Switch("help")) => {
+                return println!("{}\n\n{}", USAGE, HELP);
+            },
+            Ok(Switch("version")) => {
+                return println!("{}", env!("CARGO_PKG_VERSION"));
+            },
+            _ => unreachable!(),
+        }
+    }
+    
+    // Use the variables holding the parsed values for something
+    println!("Parsed succesfully!");
+    println!("Schema: {}", schema);
+    println!("Load: {:?}", load);
+    println!("Save: {:?}", save);
+    println!("Edit: {:?}", edit);
+    
+    if let Some(path) = edit {
+        if load.is_some() {
+            return println!("Error: --edit overwrites --load");
+        }
+        if save.is_some() {
+            return println!("Error: --edit overwrites --save");
+        }
+        load = Some(path);
+        save = Some(path);
+    }
+    
+    start_editor(schema, load, save);
+}
